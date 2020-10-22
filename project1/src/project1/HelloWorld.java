4ea4b8c8748e4e6ebdae8afb468be81bde1f9fd5
19jy0126 <19jy0126@PC32.jynet2.jec.ac.jp>
2020-10-22 12:09:44 +0900

    add HelloJavaを表示する行を追加

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -3,8 +3,8 @@ package project1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-
+		System.out.println("HelloWorld");
+		System.out.println("HelloJava");
 	}
 
 }
